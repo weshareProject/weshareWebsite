2288d93ff180e22cf180e7f69e600fc9437bba0b
YuxHubert <whjwnb@gmail.com>
2023-10-29 22:29:13 +0800

    修改了部分语法

--- register.js
+++ register.js
@@ -1,6 +1,6 @@
 document.addEventListener("DOMContentLoaded", function() {
     const registerButton = document.getElementById("register-button");
-    registerButton.addEventListener("click", function(event) {
+    registerButton.addEventListener("click", async function(event) {
         event.preventDefault();
 
         // 获取用户名、电子邮件和密码等输入的值
@@ -9,29 +9,34 @@ document.addEventListener("DOMContentLoaded", function() {
         const password = document.getElementById("password").value;
 
         // 创建一个包含用户信息的JavaScript对象
-        // 这个上周好像说了，但是忘记了，等后端完成后，再次修改
+        // 后续补充
         const userData = {
             username: username,
             email: email,
             password: password
         };
 
-        // 使用fetch将数据传输给后端（需要替换成实际的后端API）
-        fetch('your-backend-api-url', {
-            method: 'POST',
-            body: JSON.stringify(userData),
-            headers: {
-                'Content-Type': 'application/json'
+        try {
+            // 使用fetch将数据传输给后端（替换成实际的后端API）
+            const response = await fetch('your-backend-api-url', {
+                method: 'POST',
+                body: JSON.stringify(userData),
+                headers: {
+                    'Content-Type': 'application/json'
+                }
+            });
+
+            if (response.ok) {
+                const data = await response.json();
+                // 在这里可以处理后端返回的响应
+                console.log(data);
+            } else {
+                // 处理请求失败
+                console.error('Request failed with status:', response.status);
             }
-        })
-        .then(response => response.json())
-        .then(data => {
-            // 在这里可以处理后端返回的响应
-            console.log(data);
-        })
-        .catch(error => {
-            // 处理错误
+        } catch (error) {
+            // 处理其他错误
             console.error('Error:', error);
-        });
+        }
     });
 });
