4b3c9774f74af88fe0b70516754fcddbe55d9c05
testman <testman@qq.com>
2023-12-11 19:55:49 +0800

    前端代码（只有登陆和主页面）

---
+++ weshare-box/src/main.js
@@ -0,0 +1,24 @@
+import '@/assets/main.css'
+import 'animate.css';
+import 'nprogress/nprogress.css'
+
+import { createApp } from 'vue'
+import App from '@/App.vue'
+// 导入路由
+import router from '@/router'
+// 导入全局路由守卫
+import '@/permission'
+// 导入 Element Plus 图标
+import * as ElementPlusIconsVue from '@element-plus/icons-vue'
+
+const app = createApp(App)
+
+// 应用路由
+app.use(router)
+
+// 引入图标
+for (const [key, component] of Object.entries(ElementPlusIconsVue)) {
+    app.component(key, component)
+}
+
+app.mount('#app')
